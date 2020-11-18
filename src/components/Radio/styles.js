0888cfc3b23de28e8a316de20c339ed9f29a7456
Talita Galdino <talita.gouveia@ccc.ufcg.edu.br>
2020-11-18 18:05:17 -0300

    Componente de cadastro

---
+++ src/components/Radio/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const RadioContainer = styled.div`
+	width: 80%;
+	padding: 30px;
+`;
+
+export const StyledCheckbox = styled.input`
+	width: 23px;
+	height: 23px;
+	border-radius: 50%;
+`;
+
+export const Label = styled.label`
+	font-family: 'Rubik', sans-serif;
+	font-size: 20px;
+	color: #000;
+	margin-left: 10px;
+`;
